935695fec4033c6e92779563c5dc6170a709ecef
AdityaChaple15 <adityachaple02>
2025-10-24 13:02:14 +0530

    Added more table for review table

--- UberReviewService/src/main/java/com/example/UberReviewService/models/Reviews.java
+++ UberReviewService/src/main/java/com/example/UberReviewService/models/Reviews.java
@@ -1,11 +1,33 @@
 package com.example.UberReviewService.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 
 @Entity
+@Table(name = "bookingreview")
 public class Reviews {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int Id;
+    @Column(nullable = false)
+    String content;
+
+    @Column(nullable = true)
+    float rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    Date updatedAt;
 }
